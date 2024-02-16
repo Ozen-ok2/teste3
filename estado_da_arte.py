68432a53d2b2f40a11f105f07592b5059f66b40d
Yago Alexandre <77808082+yagoalexandre00@users.noreply.github.com>
2024-02-16 14:24:05 +0000

    add texto de estado da arte

--- estado_da_arte.py
+++ estado_da_arte.py
@@ -3,3 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Estado da Arte", page_icon="frontend/imagens/gen.ico")
+
+st.title("Estado da Arte")
+st.header("Tanques de armazenamento de água em residências")
+st.text("Nos tanques de armazenamento de água em residências, os sensores ultrassônicos, como o HC-SR04, desempenham um papel crucial na medição e no monitoramento dos níveis de água. Esses sensores oferecem uma solução eficiente e confiável para garantir que os tanques estejam sempre abastecidos adequadamente, permitindo que os moradores utilizem a água de forma consciente e evite interrupções no fornecimento.")
