e12dd48a926b509b738d1752da7dc34830e9dc2b
yago <goltarayago@gmail.com>
2024-02-16 10:25:53 -0300

    mudando nomes das colunas

--- yago.py
+++ yago.py
@@ -8,4 +8,4 @@ st.set_page_config(page_title="Home", page_icon="frontend/imagens/gen.ico")
 data = pd.read_csv('data.csv').rename({'Timestamp': 'timestamp', 'Distancia (cm)' : 'distancia', 'Duracao (ms)': 'duracao', 'Volume (ml)' : 'volume'})
 
 st.subheader("Variação ao Longo do Tempo")
-st.line_chart(data.set_index('duracao'), )  # Linha para cada coluna, indexado pelo timestamp
+st.line_chart(data.set_index('duracao'))  # Linha para cada coluna, indexado pelo timestamp
