d965dc120166c374f9cae677eb92f2261c11217c
Yago Alexandre <77808082+yagoalexandre00@users.noreply.github.com>
2024-02-16 14:35:05 +0000

    aadd text

--- estado_da_arte.py
+++ estado_da_arte.py
@@ -6,4 +6,4 @@ st.set_page_config(page_title="Estado da Arte", page_icon="frontend/imagens/gen.
 
 st.title("Estado da Arte")
 st.header("Tanques de armazenamento de água em residências")
-st.text("Nos tanques de armazenamento de água em residências, os sensores ultrassônicos, como o HC-SR04, desempenham um papel crucial na medição e no monitoramento dos níveis de água. Esses sensores oferecem uma solução eficiente e confiável para garantir que os tanques estejam sempre abastecidos adequadamente, permitindo que os moradores utilizem a água de forma consciente e evite interrupções no fornecimento.")
+st.text_area("Nos tanques de armazenamento de água em residências, os sensores ultrassônicos, como o HC-SR04, desempenham um papel crucial na medição e no monitoramento dos níveis de água. Esses sensores oferecem uma solução eficiente e confiável para garantir que os tanques estejam sempre abastecidos adequadamente, permitindo que os moradores utilizem a água de forma consciente e evite interrupções no fornecimento.", )
