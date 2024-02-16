87bae825b44aad5427b80ad310b1b1376eb4cef5
yago <goltarayago@gmail.com>
2024-02-16 10:25:13 -0300

    mudando nomes das colunas

--- yago.py
+++ yago.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 st.set_page_config(page_title="Home", page_icon="frontend/imagens/gen.ico")
 
 # Dados
-data = pd.read_csv('data.csv')
+data = pd.read_csv('data.csv').rename({'Timestamp': 'timestamp', 'Distancia (cm)' : 'distancia', 'Duracao (ms)': 'duracao', 'Volume (ml)' : 'volume'})
 
 st.subheader("Variação ao Longo do Tempo")
-st.line_chart(data.set_index('Duracao (ms)'), )  # Linha para cada coluna, indexado pelo timestamp
+st.line_chart(data.set_index('duracao'), )  # Linha para cada coluna, indexado pelo timestamp
